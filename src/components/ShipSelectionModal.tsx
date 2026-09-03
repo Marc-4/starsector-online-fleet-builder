6a732c1e20ef6beba86d807a751d72184d9eb8fa
Marc-4 <blaze.verdugo.s@gmail.com>
2026-09-03 18:25:08 +0800

    chore: style polish

--- src/components/ShipSelectionModal.tsx
+++ src/components/ShipSelectionModal.tsx
@@ -44,15 +44,18 @@ export default function ShipSelectionModal({
             return (
               <div
                 key={i}
-                className="flex relative items-center justify-center w-52 shrink-0 h-52 cursor-pointer hover:bg-[rgba(255,255,255)]"
+                className="group flex relative items-center justify-center w-52 shrink-0 h-52 cursor-pointer hover:bg-white/50"
               >
                 <img
                   src={`/ships${ship.spriteName}`}
                   alt="ship sprite"
-                  className="max-w-full max-h-full object-center object-contain"
+                  className="max-w-full max-h-full brightness-80 group-hover:brightness-100 object-center object-contain"
                 />
-                {/*<h1 className="absolute left-1 top-1">{ship.hullName}-class</h1>*/}
-                <h1 className="absolute left-1 top-1">{ship.spriteName}</h1>
+                <h1 className="absolute left-1 top-1 text-cyan-400 text-bold text-shadow-[0_1px_0px_rgba(0,0,0,1)] shadow-black">
+                  {!ship.hullName ? "¯\\(ツ)/¯" :
+                    `${ship.hullName}-class`
+                  }
+                </h1>
               </div>
             )
           })}
