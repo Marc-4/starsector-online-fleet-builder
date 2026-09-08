612c4059492ba27e5f71d817f5d82376a4e6b4d7
Marc-4 <blaze.verdugo.s@gmail.com>
2026-09-08 15:19:15 +0800

    feat: smaller add ships button text for small screens

--- src/components/addShipBtn.tsx
+++ src/components/addShipBtn.tsx
@@ -15,7 +15,7 @@ export default function AddShipButton() {
         className={`w-full hover:border-cyan-200 border-transparent border h-63 max-2xl:h-57.5 max-xl:h-47.75
           max-lg:h-38.25 max-md:h-28.75 max-sm:h-24 shrink-0 text-2xl font-bold cursor-pointer text-cyan-200`}
       >
-        <p className="text-4xl">+</p>
+        <p className="text-4xl max-md:text-xl">+</p>
         <p>Add Ships</p>
       </button>
       {isShipSelectionModalOpen && (
