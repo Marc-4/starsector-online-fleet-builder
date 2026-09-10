09dba8dedb271fbb6bfd2d02f08ad7fab1bba5a2
Marc-4 <blaze.verdugo.s@gmail.com>
2026-09-10 16:35:25 +0800

    fix: static build mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from "vite"
+import { devtools } from "@tanstack/devtools-vite"
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
-import svgr from 'vite-plugin-svgr'
+import viteReact from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import { nitro } from "nitro/vite"
+import svgr from "vite-plugin-svgr"
+
+const REPO_NAME = "starsector-online-fleet-builder"
 
 const config = defineConfig({
+  base: `/${REPO_NAME}/`,
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    nitro({
+      static: true,
+      prerender: {
+        crawlLinks: true,
+        routes: ["/"]
+      },
+      rollupConfig: { external: [/^@sentry\//] }
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    svgr(),
-  ],
+    svgr()
+  ]
 })
 
 export default config
